b978c310f62001a2f860bd1c7439f08ad2bee1f9
Ayush Yadav <aayush.sang@gmail.com>
2021-08-05 21:19:21 +0530

    files I-O

---
+++ Tricks/Input-Output from Files.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+
+    // For getting input from input.txt file
+    freopen("input.txt", "r", stdin);
+
+    // Printing the Output to output.txt file
+    freopen("output.txt", "w", stdout);
+
+#endif
+    return 0;
+}
+
+// Can also pass intputs in files to cmd
+// Just use cin and cout in main
+
+// Commands:
+// g++ test_code.cpp
+// a.exe < input_file > output_file
