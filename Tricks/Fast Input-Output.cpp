58afbf06c2312053a39817225cff6fdbbc427d3f
Ayush Yadav <aayush.sang@gmail.com>
2021-08-05 21:19:11 +0530

    fast input output

---
+++ Tricks/Fast Input-Output.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // Faster Input
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // Use "/n" instead of endl
+    cout << "/n";
+
+    return 0;
+}
